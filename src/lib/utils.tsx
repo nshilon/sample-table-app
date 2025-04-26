abdcd84911d401122288a2005bde178638e8ddf3
niroshilon <nir.shilon@gmail.com>
2025-04-26 14:51:04 +0300

    utils/deepMerge

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -8,3 +8,18 @@ export const debounce = (func: any, delay: number) => {
         }, delay);
     }
 }
+
+export const deepMerge = (target: any, source: any) => {
+
+    for (const key in source) {
+        if (source.hasOwnProperty(key)) {
+            if (typeof source[key] === 'object' && typeof target[key] === 'object') {
+                target[key] = deepMerge(target[key], source[key]);
+            } else {
+                target[key] = source[key];
+            }
+        }
+    }
+
+    return target;
+}
